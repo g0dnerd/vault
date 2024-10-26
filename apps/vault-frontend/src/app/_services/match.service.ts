fbc0d2f8709b143d26d14949604b8536268d6240
Paul Kukowski <kukowskipaul@gmail.com>
2024-10-26 10:56:53 +0200

    Corrected `confirmResult` to use a PATCH instead of a POST request

--- apps/vault-frontend/src/app/_services/match.service.ts
+++ apps/vault-frontend/src/app/_services/match.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { environment } from '../../environments/environment';
 import { API_ROUTES, Match, Result } from '@vault/shared';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -12,14 +12,10 @@ export class MatchService {
   private apiUrl = `${environment.apiUrl}${API_ROUTES.MATCHES}`;
   private resultApiUrl = `${environment.apiUrl}${API_ROUTES.RESULTS}`;
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) {}
 
   getAllMatches(): Observable<Match[]> {
     return this.http.get<Match[]>(this.apiUrl);
-  }
-
-  getCurrentMatchByUserId(userId: number, tournamentId: number): Observable<Match> {
-    return this.http.get<Match>(`${this.apiUrl}/${userId}/current/${tournamentId}`);
   }
 
   getCurrentUserCurrentMatch(tournamentId: number): Observable<Match> {
@@ -33,5 +29,12 @@ export class MatchService {
   /// Reports a result to the API and returns the API response.
   reportResult(result: Result): Observable<Match> {
     return this.http.post<Match>(this.resultApiUrl, result);
+  }
+
+  // POSTs a request to confirm the match for `matchId`
+  confirmResult(matchId: number): Observable<Match> {
+    return this.http.patch<Match>(`${this.resultApiUrl}/${matchId}`, {
+      confirmed: true,
+    });
   }
 }
