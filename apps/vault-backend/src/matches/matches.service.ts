e706f164ce0fe6e426b81ebd8304cf317578cb9d
Paul Kukowski <kukowskipaul@gmail.com>
2024-10-26 10:59:32 +0200

    Expanded the queries for currentMatch GETs to return more information on opponent and result

--- apps/vault-backend/src/matches/matches.service.ts
+++ apps/vault-backend/src/matches/matches.service.ts
@@ -2,11 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { CreateMatchDto } from './dto/create-match.dto';
 import { UpdateMatchDto } from './dto/update-match.dto';
 import { PrismaService } from '../prisma/prisma.service';
-import { Match } from '@prisma/client';
 
 @Injectable()
 export class MatchesService {
-  constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
   create(createMatchDto: CreateMatchDto) {
     return this.prisma.match.create({ data: createMatchDto });
@@ -17,6 +16,7 @@ export class MatchesService {
   }
 
   async findCurrentMatchByUserId(userId: number, tournamentId: number) {
+    // Find the active draft player for the given user and tournament
     const draftPlayer = await this.prisma.draftPlayer.findFirst({
       where: {
         enrollment: {
@@ -27,27 +27,47 @@ export class MatchesService {
           started: true,
           finished: false,
         },
-      }
+      },
     });
 
+    // Find the active match for that draft player
     const game = await this.prisma.match.findFirst({
       where: {
-        OR: [
-          { player1Id: draftPlayer.id },
-          { player2Id: draftPlayer.id },
-        ],
+        OR: [{ player1Id: draftPlayer.id }, { player2Id: draftPlayer.id }],
         round: {
           started: true,
           finished: false,
-        }
+        },
       },
+      // Include userId and username for both players
+      // so that we can return information on the opponent
       include: {
-        player1: { select: { enrollment: { select: { user: { select: { username: true } } } } } },
-        player2: { select: { enrollment: { select: { user: { select: { username: true } } } } } },
-      }
+        player1: {
+          select: {
+            enrollment: {
+              select: {
+                userId: true,
+                user: { select: { username: true } },
+              },
+            },
+          },
+        },
+        player2: {
+          select: {
+            enrollment: {
+              select: {
+                userId: true,
+                user: { select: { username: true } },
+              },
+            },
+          },
+        },
+      },
     });
 
-    const opponent = draftPlayer.id === game.player1Id ? game.player2 : game.player1;
+    // The opponent is the other player in the game
+    const opponent =
+      draftPlayer.id === game.player1Id ? game.player2 : game.player1;
 
     const result = await this.prisma.result.findUnique({
       where: {
@@ -58,9 +78,19 @@ export class MatchesService {
         player2Wins: true,
         confirmed: true,
         result: true,
-      }
+        reportedBy: {
+          select: {
+            enrollment: {
+              select: {
+                userId: true,
+              },
+            },
+          },
+        },
+      },
     });
 
+    // Return the match, the result and the opponent
     return {
       game,
       result,
@@ -72,18 +102,36 @@ export class MatchesService {
     const game = await this.prisma.match.findUnique({
       where: { id },
       include: {
-        player1: { select: { enrollment: { select: { user: { select: { username: true } } } } } },
-        player2: { select: { enrollment: { select: { user: { select: { username: true } } } } } },
+        player1: {
+          select: {
+            enrollment: {
+              select: {
+                userId: true,
+                user: { select: { username: true } },
+              },
+            },
+          },
+        },
+        player2: {
+          select: {
+            enrollment: {
+              select: {
+                userId: true,
+                user: { select: { username: true } },
+              },
+            },
+          },
+        },
         round: {
           select: {
             draft: {
               select: {
-                phase: true
-              }
-            }
-          }
-        }
-      }
+                phase: true,
+              },
+            },
+          },
+        },
+      },
     });
 
     const draftPlayer = await this.prisma.draftPlayer.findFirstOrThrow({
@@ -96,15 +144,28 @@ export class MatchesService {
           started: true,
           finished: false,
         },
-      }
+      },
     });
 
-    const opponent = draftPlayer.id === game.player1Id ? game.player2 : game.player1;
+    const opponent =
+      draftPlayer.id === game.player1Id ? game.player2 : game.player1;
 
     const result = await this.prisma.result.findUnique({
       where: {
         matchId: game.id,
-      }
+      },
+      // TODO: Just return the reported by draft player ID instead and have that in redux in the frontend
+      include: {
+        reportedBy: {
+          select: {
+            enrollment: {
+              select: {
+                userId: true,
+              },
+            },
+          },
+        },
+      },
     });
 
     return {
