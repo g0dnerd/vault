bf16e3df4dfb01cc28e253bffcd1d675fdb9ec23
Paul Kukowski <kukowskipaul@gmail.com>
2024-10-26 11:06:52 +0200

    Finished result reporting and added `@defer` blocks

--- apps/vault-frontend/src/app/tournaments/dashboard/match-panel.component.ts
+++ apps/vault-frontend/src/app/tournaments/dashboard/match-panel.component.ts
@@ -1,11 +1,20 @@
 import { Component, Input, numberAttribute, OnInit } from '@angular/core';
 import { KeyValuePipe, NgClass, NgIf } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { firstValueFrom, Observable, of } from 'rxjs';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { first, firstValueFrom, Observable, of } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { PushPipe } from '@ngrx/component';
 
-import { MatchState, selectCurrentMatch, selectCurrentMatchId } from '../../store';
+import {
+  MatchState,
+  selectCurrentMatch,
+  selectCurrentMatchId,
+} from '../../store';
 import { initCurrent, reportResult } from '../../store/actions/match.actions';
 import { AlertService } from '../../_services';
 import { Match } from '@vault/shared';
@@ -22,11 +31,12 @@ export class MatchPanelComponent implements OnInit {
   loading = false;
   submitted = false;
   currentMatch$: Observable<Match | null> = of(null);
+  currentMatchId$: Observable<number> = of(0);
 
   constructor(
     private formBuilder: FormBuilder,
     private readonly matchStore$: Store<MatchState>,
-    private readonly alertService: AlertService,
+    private readonly alertService: AlertService
   ) {
     this.form = this.formBuilder.group({
       player1Wins: [
@@ -38,16 +48,15 @@ export class MatchPanelComponent implements OnInit {
         [Validators.required, Validators.min(0), Validators.max(2)],
       ],
     });
-
   }
 
   @Input({ transform: numberAttribute }) tournamentId = 0;
 
   async ngOnInit() {
-    // Initialize result reporting form
-
-    this.matchStore$.dispatch(initCurrent({ tournamentId: this.tournamentId }));
     this.currentMatch$ = this.matchStore$.select(selectCurrentMatch);
+    this.currentMatchId$ = this.matchStore$
+      .select(selectCurrentMatchId)
+      .pipe(first((id) => id != null));
 
     this.form.setValue({
       player1Wins: 0,
@@ -65,28 +74,34 @@ export class MatchPanelComponent implements OnInit {
 
     if (this.form.invalid) return;
 
-    const mId$ = this.matchStore$.select(selectCurrentMatchId);
-    const matchId = await firstValueFrom(mId$);
-    console.log(matchId);
+    const result =
+      this.f['player1Wins'].value === this.f['player2Wins'].value
+        ? 0
+        : this.f['player1Wins'] > this.f['player2Wins']
+        ? -1
+        : 1;
 
-    const result = this.f['player1Wins'].value === this.f['player2Wins'].value ? 0 : this.f['player1Wins'] > this.f['player2Wins'] ? -1 : 1;
-    this.matchStore$.dispatch(reportResult({
-      result: {
-        player1Wins: this.f['player1Wins'].value,
-        player2Wins: this.f['player2Wins'].value,
-        matchId,
-        result,
-      }
-    }));
+    // TODO: Handle error responses
+    const matchId = await firstValueFrom(this.currentMatchId$);
+    this.matchStore$.dispatch(
+      reportResult({
+        result: {
+          player1Wins: this.f['player1Wins'].value,
+          player2Wins: this.f['player2Wins'].value,
+          matchId,
+          result,
+        },
+      })
+    );
     this.loading = false;
 
     this.currentMatch$.subscribe((game) => {
       console.log(JSON.stringify(game));
-    })
+    });
   }
 
   onConfirm() {
     // TODO: Result confirmation service
-    console.log("foo");
+    console.log('foo');
   }
 }
--- apps/vault-frontend/src/app/tournaments/dashboard/tournament-dashboard.component.ts
+++ apps/vault-frontend/src/app/tournaments/dashboard/tournament-dashboard.component.ts
@@ -5,28 +5,43 @@ import { Observable, of } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { PushPipe } from '@ngrx/component';
 
-import { MatchPanelComponent } from './match-panel.component';
 import { Tournament } from '@vault/shared';
-import { DraftPanelComponent } from "./draft-panel.component";
-import { selectSelectedTournament, TournamentsState } from '../../store';
+import { MatchPanelComponent } from './match-panel.component';
+import { DraftPanelComponent } from './draft-panel.component';
+import {
+  MatchAppState,
+  selectSelectedTournament,
+  TournamentAppState,
+} from '../../store';
 import { selectTournament } from '../../store/actions/tournaments.actions';
+import { initCurrent } from '../../store/actions/match.actions';
 
 @Component({
   selector: 'app-tournament-dashboard',
   standalone: true,
-  imports: [NgIf, RouterLink, MatchPanelComponent, DraftPanelComponent, PushPipe],
+  imports: [
+    NgIf,
+    RouterLink,
+    MatchPanelComponent,
+    DraftPanelComponent,
+    PushPipe,
+  ],
   templateUrl: './tournament-dashboard.component.html',
   styleUrl: './tournament-dashboard.component.css',
 })
 export class TournamentDashboardComponent implements OnInit {
   tournament$: Observable<Tournament | null> = of(null);
 
-  constructor(private readonly store$: Store<TournamentsState>) { }
+  constructor(
+    private readonly tournamentStore$: Store<TournamentAppState>,
+    private readonly matchStore$: Store<MatchAppState>
+  ) {}
 
   @Input({ transform: numberAttribute }) id = 0;
 
   ngOnInit() {
-    this.store$.dispatch(selectTournament({ id: this.id }));
-    this.tournament$ = this.store$.select(selectSelectedTournament);
+    this.tournamentStore$.dispatch(selectTournament({ id: this.id }));
+    this.matchStore$.dispatch(initCurrent({ tournamentId: this.id }));
+    this.tournament$ = this.tournamentStore$.select(selectSelectedTournament);
   }
 }
