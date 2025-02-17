f835ce3b8fb088e91370094ef53133edf69b0c32
Paul Kukowski <kukowskipaul@gmail.com>
2025-02-17 09:02:20 +0100

    Start work on `DraftPlayer` frontend service

---
+++ apps/vault-frontend/src/app/_services/draft-player.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { API_ROUTES, Player } from '@vault/shared';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DraftPlayerService {
+  private apiUrl = `${environment.apiUrl}${API_ROUTES.PLAYERS}`;
+
+  constructor(private readonly http: HttpClient) {}
+
+  getAllPlayers(): Observable<Player[]> {
+    return this.http.get<Player[]>(`${this.apiUrl}`);
+  }
+
+  getPlayersForTournament(tournamentId: number): Observable<Player[]> {
+    return this.http.get<Player[]>(`${this.apiUrl}/tournament/${tournamentId}`);
+  }
+
+  checkIn(player: Player, playerId: number): Observable<Player> {
+    player = {
+      ...player,
+      checkedIn: true,
+    };
+    return this.http.patch<Player>(`${this.apiUrl}/patch/${playerId}`, player);
+  }
+
+  checkOut(player: Player, playerId: number): Observable<Player> {
+    player = {
+      ...player,
+      checkedOut: true,
+    };
+    return this.http.patch<Player>(`${this.apiUrl}/patch/${playerId}`, player);
+  }
+}
