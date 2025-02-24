48184a46ec0d0b5799efebdaf98f73d5443ca185
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-02-24 18:34:55 +0100

    Expand league page stub

--- apps/vault-frontend/src/app/leagues/leagues-home.component.ts
+++ apps/vault-frontend/src/app/leagues/leagues-home.component.ts
@@ -1,4 +1,4 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { PushPipe } from '@ngrx/component';
 import { Store } from '@ngrx/store';
@@ -9,19 +9,28 @@ import {
   AuthAppState,
   selectAuthUser,
   selectAvailableLeagues,
+  selectEnrolledLeagues,
   State,
 } from '../_store';
 import {
   initializeAllTournaments,
   initializeAvailableTournaments,
+  initializeEnrolledTournaments,
   register,
 } from '../_store/actions/tournaments.actions';
+import { LeagueDetailCardComponent } from './league-detail-card.component';
 import { RegisterPanelComponent } from '../tournaments/available-tournaments';
 
 @Component({
   selector: 'app-leagues-home',
   standalone: true,
-  imports: [NgFor, PushPipe, RegisterPanelComponent],
+  imports: [
+    LeagueDetailCardComponent,
+    NgIf,
+    NgFor,
+    PushPipe,
+    RegisterPanelComponent,
+  ],
   templateUrl: './leagues-home.component.html',
   styleUrl: './leagues-home.component.css',
 })
@@ -34,10 +43,14 @@ export class LeaguesHomeComponent implements OnInit {
   readonly availableLeagues$: Observable<Tournament[]> = this.store$.select(
     selectAvailableLeagues
   );
+  readonly enrolledLeagues$: Observable<Tournament[]> = this.store$.select(
+    selectEnrolledLeagues
+  );
 
   ngOnInit() {
     this.store$.dispatch(initializeAllTournaments());
     this.store$.dispatch(initializeAvailableTournaments());
+    this.store$.dispatch(initializeEnrolledTournaments());
   }
 
   registerTournament(registrationData: Enrollment) {
