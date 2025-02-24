2822e1fdd33c814a4c2aee803b97fd31f5aa3b37
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-02-24 16:37:10 +0100

    Remove `Scorecard` models

--- apps/vault-backend/src/draft-scorecards/dto/update-draft-scorecard.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateDraftScorecardDto } from './create-draft-scorecard.dto';
-
-export class UpdateDraftScorecardDto extends PartialType(
-  CreateDraftScorecardDto
-) {}
--- apps/vault-backend/src/scorecards/dto/update-scorecard.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateScorecardDto } from './create-scorecard.dto';
-
-export class UpdateScorecardDto extends PartialType(CreateScorecardDto) {}
