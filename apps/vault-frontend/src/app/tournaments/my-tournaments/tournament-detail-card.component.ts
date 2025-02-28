63e6b4b000d341b38ceeeb55af9c7a9b729e4975
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-02-28 23:11:19 +0100

    Streamline component ID inputs

--- apps/vault-frontend/src/app/tournaments/my-tournaments/tournament-detail-card.component.ts
+++ apps/vault-frontend/src/app/tournaments/my-tournaments/tournament-detail-card.component.ts
@@ -1,19 +1,28 @@
-import {
-  Component,
-  numberAttribute,
-  Input,
-} from '@angular/core';
+import { Component, Input, numberAttribute, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { PushPipe } from '@ngrx/component';
+import { Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+
+import { selectTournamentById, State } from '../../_store';
+import { Tournament } from '@vault/shared';
 
 @Component({
   selector: 'app-tournament-detail-card',
   standalone: true,
-  imports: [RouterLink],
+  imports: [PushPipe, RouterLink],
   templateUrl: './tournament-detail-card.component.html',
   styleUrl: './tournament-detail-card.component.css',
 })
-export class TournamentDetailCardComponent {
+export class TournamentDetailCardComponent implements OnInit {
   @Input({ required: true, transform: numberAttribute }) tournamentId = 0;
-  @Input({ required: true }) tournamentName = '';
-  @Input({ required: true, transform: numberAttribute }) tournamentCapacity = 0;
+  tournament$: Observable<Tournament | undefined> = of(undefined);
+
+  constructor(private readonly store$: Store<State>) {}
+
+  ngOnInit() {
+    this.tournament$ = this.store$.select(
+      selectTournamentById(this.tournamentId)
+    );
+  }
 }
