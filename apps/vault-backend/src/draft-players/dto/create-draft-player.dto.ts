f348142714a36c65365f83ca2093c9ade53000bf
Paul Kukowski <kukowskipaul@gmail.com>
2025-03-03 15:08:04 +0100

    Add pairing and drop flags to `DraftPlayer` model, remove `Result` field from `Match` model

--- apps/vault-backend/src/draft-players/dto/create-draft-player.dto.ts
+++ apps/vault-backend/src/draft-players/dto/create-draft-player.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNumber, IsOptional, IsPositive } from 'class-validator';
+import {
+  IsBoolean,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+} from 'class-validator';
 
 export class CreateDraftPlayerDto {
   @IsNumber()
@@ -21,4 +27,24 @@ export class CreateDraftPlayerDto {
   @IsOptional()
   @ApiProperty({ required: false })
   checkedOut?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty({ required: false })
+  bye?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty({ required: false })
+  hadBye?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty({ required: false })
+  dropped?: boolean;
+
+  @IsInt()
+  @IsOptional()
+  @ApiProperty({ required: false })
+  seat?: number;
 }
