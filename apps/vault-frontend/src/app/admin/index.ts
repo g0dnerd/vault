49660e5fb4b139a723fcf8901d66e9db3cef0c37
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-03-04 22:54:47 +0100

    Remove obsolete `AdminMatchPanelComponent`

--- apps/vault-frontend/src/app/admin/index.ts
+++ apps/vault-frontend/src/app/admin/index.ts
@@ -2,4 +2,3 @@ export * from './admin-dashboard.component';
 export * from './admin-tournament-dashboard.component';
 export * from './admin-tournament-list.component';
 export * from './draft/admin-draft-panel.component';
-export * from './draft/admin-match-panel.component';
